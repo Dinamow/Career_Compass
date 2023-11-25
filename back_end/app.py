db6ad65783e88d1a0b2a4fc8d11d8e1ad4bd6979
DINAMOW <meemoo102039@gmail.com>
2023-11-25 10:25:49 +0200

    fix pycodestyle

--- back_end/app.py
+++ back_end/app.py
@@ -10,6 +10,7 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
 
+
 @app.route('/')
 def index():
     """index 0"""
@@ -18,17 +19,22 @@ def index():
     __subject = "Career Compass"
     __body = "this is the body"
     __recipients = ['meemoo102039@gmail.com']
-    __message = Message(subject=__subject, recipients=__recipients, body=__body)
+    __message = Message(subject=__subject,
+                        recipients=__recipients,
+                        body=__body)
     __mail.send(__message)
     return jsonify(storage.truth())
+
 
 @app.route('/api/v1/questions')
 def home():
     return jsonify(storage.getq())
 
+
 @app.route('/api/v1/statistics')
 def statistics():
     return jsonify(storage.getall())
+
 
 @app.route('/api/v1/result/<string:unique_url_id>')
 def unique(unique_url_id):
@@ -36,13 +42,14 @@ def unique(unique_url_id):
         return jsonify(storage.getone(unique_url_id))
     return abort(404, "Not found")
 
+
 @app.route('/api/v1/result', methods=['POST'])
 def post():
     data = request.get_json()
 
     if not data:
         return abort(400, "Not a JSON")
-    
+
     if storage.Eexists(data['email']):
         return abort(400, "Email already exists")
 
@@ -51,6 +58,7 @@ def post():
     storage.insert(data)
 
     return jsonify({}), 201
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5002, debug=True)
--- back_end/engin/db_storage.py
+++ back_end/engin/db_storage.py
@@ -3,9 +3,11 @@ import json
 import mysql.connector
 from uuid import uuid4
 
+
 class Storage:
+    """class that works as engine for the Career Compass project"""
     __connection = None
-    
+
     def __init__(self):
         """Initialize the storage object with data from data.json"""
 
@@ -13,7 +15,7 @@ class Storage:
         json_file_path = os.path.join(file_dir, 'data.json')
         with open(json_file_path) as file:
             self.__storage = json.load(file)
-        
+
         self.__connection = mysql.connector.connect(
             user='compass',
             password='password',
@@ -24,7 +26,8 @@ class Storage:
     def getq(self):
         """return a list of 40 questions"""
 
-        self.__sorted = sorted(self.__storage['questions'], key=lambda x: x['Intelligence_type'])
+        self.__sorted = sorted(self.__storage['questions'],
+                               key=lambda x: x['Intelligence_type'])
         self.__result = []
         self.__nums = self.ranomize()
         for i in self.__nums:
@@ -46,8 +49,7 @@ class Storage:
                 result.append(x)
 
         return result
-    
-    
+
     def getall(self):
         """return a list of all users"""
 
@@ -55,28 +57,31 @@ class Storage:
         self.__query = "SELECT COUNT(*) FROM usr"
         self.__cursor.execute(self.__query)
         self.__user_count = self.__cursor.fetchone()
-        self.__query = "SELECT COUNT(*) FROM usr WHERE quize_type = 'High school'"
+        self.__query = "SELECT COUNT(*) FROM usr WHERE \
+            quize_type = 'High school'"
         self.__cursor.execute(self.__query)
         self.__user_high = self.__cursor.fetchone()
         self.__query = "SELECT COUNT(*) FROM usr WHERE quize_type = 'graduate'"
         self.__cursor.execute(self.__query)
         self.__user_graduate = self.__cursor.fetchone()
-        self.__query = "SELECT linguistic, logical_mathematical, bodily_kinesthetic, spatial_visual, interpersonal, intrapersonal, naturalist, musical FROM usr;"
+        self.__query = "SELECT linguistic, logical_mathematical, \
+            bodily_kinesthetic, spatial_visual, interpersonal, \
+                intrapersonal, naturalist, musical FROM usr;"
         self.__cursor.execute(self.__query)
         self.__user_values = self.__cursor.fetchall()
         self.__cursor.close()
 
         self.__result = {
-            "linguistic" : 0,
-            "logical_mathematical" : 0,
-            "bodily_kinesthetic" : 0,
-            "spatial_visual" : 0,
+            "linguistic": 0,
+            "logical_mathematical": 0,
+            "bodily_kinesthetic": 0,
+            "spatial_visual": 0,
             "interpersonal": 0,
             "intrapersonal": 0,
             "naturalist": 0,
             "musical": 0
         }
-        
+
         for i in self.__user_values:
             self.__result['linguistic'] += i[0]
             self.__result['logical_mathematical'] += i[1]
@@ -93,13 +98,12 @@ class Storage:
 
         for i in self.__result:
             self.__result[i] = float(self.__result[i] / x * 100)
-        
+
         self.__result["Completed the quize"] = self.__user_count[0]
         self.__result["High school students"] = self.__user_high[0]
         self.__result["Graduates"] = self.__user_graduate[0]
 
         return self.__result
-        
 
     def getone(self, unique_url_id):
         """return a user with the given unique_url_id"""
@@ -111,16 +115,17 @@ class Storage:
         self.__cursor.close()
 
         self.__result = {}
-        
+
         keys = ['linguistic', 'logical_mathematical',
                 'bodily_kinesthetic', 'spatial_visual',
                 'interpersonal', 'intrapersonal',
                 'naturalist', 'musical']
-        
+
         for i in keys:
             self.__result[i] = self.__user[keys.index(i) + 5]
-                
-        self.__sorted_items = sorted(self.__result.items(), key=lambda x: x[1], reverse=True)
+
+        self.__sorted_items = sorted(self.__result.items(),
+                                     key=lambda x: x[1], reverse=True)
         self.__largest_keys = [self.__sorted_items[0] for
                                self.__sorted_items in
                                self.__sorted_items[:2]]
@@ -129,7 +134,7 @@ class Storage:
 
         for i in self.__largest_keys:
             self.__info[i] = self.__storage['intellegnces'][i]
-        
+
         return self.__info
 
     def exists(self, unique_url_id):
@@ -140,39 +145,54 @@ class Storage:
         self.__cursor.execute(self.__query)
         self.__user = self.__cursor.fetchone()
         self.__cursor.close()
-        
+
         return self.__user is not None
 
     def Eexists(self, Email):
         """check if the user with Email exists or not"""
-        
+
         self.__cursor = self.__connection.cursor()
         self.__query = f"SELECT * FROM usr WHERE email = '{Email}'"
         self.__cursor.execute(self.__query)
         self.__user = self.__cursor.fetchone()
         self.__cursor.close()
-        
+
         return self.__user is not None
 
     def insert(self, data):
         """insert the data to db"""
 
-        self.__command = f"INSERT INTO usr (name, email, uuid, linguistic, logical_mathematical, bodily_kinesthetic, spatial_visual, interpersonal, intrapersonal, naturalist, musical, quize_type) VALUES ('{data['name']}', '{data['email']}', '{data['uuid']}', {data['linguistic']}, {data['logical_mathematical']}, {data['bodily_kinesthetic']}, {data['spatial_visual']}, {data['interpersonal']}, {data['intrapersonal']}, {data['naturalist']}, {data['musical']}, '{data['quize_type']}')"
-        
+        self.__values = f"VALUES ('{data['name']}', \
+            '{data['email']}', '{data['uuid']}', \
+                {data['linguistic']}, {data['logical_mathematical']}, \
+                    {data['bodily_kinesthetic']}, {data['spatial_visual']}, \
+                        {data['interpersonal']}, {data['intrapersonal']}, \
+                            {data['naturalist']}, {data['musical']}, \
+                                '{data['quize_type']}')"
+        self.__command = f"INSERT INTO usr \
+            (name, email, uuid, linguistic, \
+                logical_mathematical, \
+                    bodily_kinesthetic, \
+                        spatial_visual, \
+                            interpersonal, \
+                                intrapersonal, \
+                                    naturalist, musical, \
+                                        quize_type) {self.__values}"
+
         self.__cursor = self.__connection.cursor()
         self.__cursor.execute(self.__command)
         self.__connection.commit()
         self.__cursor.close()
-    
+
     def truth(self):
         """give the truth"""
-        
+
         return {"DINAMOW": "im the best",
                 "Ahmed": "is the gayest"}
-    
+
     def sende(self, app, data):
         """send email to the user"""
-        
+
         from flask_mail import Mail
 
         app.config['MAIL_SERVER'] = 'smtp.gmail.com'
@@ -181,15 +201,17 @@ class Storage:
         app.config['MAIL_USE_SSL'] = False
         app.config['MAIL_USERNAME'] = 'career1compass@gmail.com'
         app.config['MAIL_PASSWORD'] = 'hojy yqtt moin ekqh'
-        
+
         return Mail(app)
-        
+
     def __str__(self):
         """the string representation of the storage object"""
 
-        return """This is the storage object works as engine for the Career Compass project"""
+        return """This is the storage object \
+            works as engine for the Career Compass project"""
 
     def __repr__(self):
         """the string representation of the storage object"""
 
-        return """This is the storage object works as engine for the Career Compass project"""
+        return """This is the storage object works \
+            as engine for the Career Compass project"""
